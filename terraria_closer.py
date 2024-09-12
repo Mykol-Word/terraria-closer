3ddf304ec8813a450bc93b2caec45c3d75dcd6d7
Mykol Word <michaelward1916@gmail.com>
2024-09-12 14:30:33 -0700

    Added programmable closing sequence when found

--- terraria_closer.py
+++ terraria_closer.py
@@ -1,8 +1,10 @@
 import requests
 import json
-from datetime import datetime, timezone
-import keyboard
 import os
+from datetime import datetime, timezone
+import time
+import keyboard
+import pydirectinput
 
 # Functions
 
@@ -15,11 +17,11 @@ def check_for_close_tag(close_tag, arm_time):
     request_json = json.loads(request.text)
     if "message" in request_json:
         print("Get request failed. The response was '" + request.text + ".' This is likely due to invalid keys in 'keys.json'")
+        print("Program terminating.")
         exit(1)
     for message in request_json:
         message_time = datetime.fromisoformat(message['timestamp'])
         if(close_tag in message['content'] and message_time > arm_time):
-            print("FOUND")
             return True
         
 def get_keys():
@@ -34,27 +36,57 @@ def get_keys():
 
     return key_json['user_key'], key_json['chat_key']
 
+def play_automated_sequence(automated_sequence):
+    for command in automated_sequence:
+        time.sleep(1)
+        if command == "click":
+            pydirectinput.mouseDown(button='left')
+            time.sleep(0.1)
+            pydirectinput.mouseUp(button='left')
+        else:
+            pydirectinput.moveTo(command[0], command[1])
+
 # Main Program
 
 user_id, chat_id = get_keys()
 
 main_loop = 1
 
-close_tag = input("Type your desired close tag:")
+close_tag = input("Type your desired close tag: ")
+print("")
 
-armed = input("Type 'ARM' to arm the server closer:")
-while armed != "ARM":
-    armed = input("Invalid input. Type 'ARM' to arm the server closer:")
+automated_sequence = []
+
+print("Press 'l' (L) to set a mouse location. Press 'c' to set a mouse click. Press 'e' to end your automated sequence.")
+sequence_going = True
+while sequence_going:
+    keyboard.read_key()
+    if keyboard.is_pressed('l'):
+        print(pydirectinput.position())
+        automated_sequence.append(pydirectinput.position())
+    elif keyboard.is_pressed('c'):
+        print("Click")
+        automated_sequence.append("click")
+    elif keyboard.is_pressed('e'):
+        sequence_going = False
+print("")
+
+while input("Type 'ARM' to arm the server closer: ") != "ARM":
+    print("Invalid input.")
+print("")
 
 arm_time = datetime.now(datetime.now().astimezone().tzinfo)
 arm_time = arm_time.astimezone(timezone.utc)
 
 print("Your close tag is '" + close_tag + "'")
 print("Press 'q' to unarm the server closer")
+print("")
 
 while main_loop:
     if keyboard.is_pressed('q'):
         main_loop = 0
 
     if check_for_close_tag(close_tag, arm_time):
+        play_automated_sequence(automated_sequence)
+        print("Closed: program terminating.")
         main_loop = 0
